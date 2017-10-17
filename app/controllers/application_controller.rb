057b881515bb6bb7e5b3efca492c942de7cb583d
ParthivPatel-BTC <parthiv.patel@botreetechnologies.com>
2017-10-17 15:22:02 +0530

    Fixed error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def current_store_info(store)
     connection = Bigcommerce::Connection.build(Bigcommerce::Config.new(store_hash: store.store_hash , client_id: ENV['BC_CLIENT_ID'], access_token: store.access_token))
-    JSON.parse(connection.get("/stores/#{store_hash}/v2/store").body)
+    JSON.parse(connection.get("/stores/#{store.store_hash}/v2/store").body)
   end
 
 end
